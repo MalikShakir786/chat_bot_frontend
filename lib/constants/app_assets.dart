c7a2f36444bf08e19f1ece03b871fd4d11110a3b
MalikShakir786 <139993713+MalikShakir786@users.noreply.github.com>
2026-06-13 17:08:52 +0500

    assets added

---
+++ lib/constants/app_assets.dart
@@ -0,0 +1,32 @@
+class AppAssets {
+
+  // Icons
+  static const String addFiles = 'assets/icons/add_files.png';
+  static const String appLogo = 'assets/icons/app_logo.png';
+
+  static const String chatFilled = 'assets/icons/chat_filled.png';
+  static const String chatOutlined = 'assets/icons/chat_outlined.png';
+
+  static const String docs = 'assets/icons/docs.png';
+  static const String pdf = 'assets/icons/pdf.png';
+  static const String txt = 'assets/icons/txt.png';
+
+  static const String email = 'assets/icons/email.png';
+  static const String lock = 'assets/icons/lock.png';
+
+  static const String eyeHide = 'assets/icons/eye_hide.png';
+  static const String eyeShow = 'assets/icons/eye_show.png';
+
+  static const String google = 'assets/icons/google.png';
+  static const String microsoft = 'assets/icons/microsoft.png';
+
+  static const String homeFilled = 'assets/icons/home_filled.png';
+  static const String homeOutlined = 'assets/icons/home_outlined.png';
+
+  static const String menu = 'assets/icons/menu.png';
+  static const String send = 'assets/icons/send.png';
+
+  static const String upload = 'assets/icons/upload.png';
+  static const String user = 'assets/icons/user.png';
+
+}
